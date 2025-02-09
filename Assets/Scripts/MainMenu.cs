b6b4c41c1167b451b856d5d9fc5035d2af9ea1c6
Nils Hammond <nils.hammond@student.howest.be>
2025-02-09 02:01:58 +0100

    Main Menu

---
+++ Assets/Scripts/MainMenu.cs
@@ -0,0 +1,39 @@
+using UnityEngine;
+using UnityEngine.UIElements;
+using UnityEngine.SceneManagement;
+
+public class MainMenu : MonoBehaviour
+{
+    private VisualElement _ui;
+    protected Button _startButton;
+    protected Button _optionsButton;
+
+    protected virtual void OnEnable()
+    {
+        _ui = GetComponent<UIDocument>().rootVisualElement;
+        _startButton = _ui.Q("StartButton") as Button;
+        if (_startButton != null)
+            _startButton.RegisterCallback<ClickEvent>(OnStartButtonClick);
+
+        _optionsButton = _ui.Q("OptionsButton") as Button;
+        if (_optionsButton != null)
+            _optionsButton.RegisterCallback<ClickEvent>(OnOptionsButtonClick);
+    }
+
+    void OnDisable()
+    {
+        if (_startButton != null)
+            _startButton.UnregisterCallback<ClickEvent>(OnStartButtonClick);
+        if (_optionsButton != null)
+            _optionsButton.UnregisterCallback<ClickEvent>(OnOptionsButtonClick);
+    }
+
+    void OnStartButtonClick(ClickEvent evt)
+    {
+        SceneManager.LoadScene("SampleScene");
+    }
+    void OnOptionsButtonClick(ClickEvent evt)
+    {
+
+    }
+}
