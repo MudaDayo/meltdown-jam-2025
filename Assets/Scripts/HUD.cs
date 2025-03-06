0ebe51bb1de81dbdbfc9c6e190dabbb97f491ffd
Dan Chicu <38363167+MudaDayo@users.noreply.github.com>
2025-03-06 16:36:27 +0100

    timer working for end screen

--- Assets/Scripts/HUD.cs
+++ Assets/Scripts/HUD.cs
@@ -1,40 +1,67 @@
 using UnityEngine;
 using UnityEngine.UI;
 using UnityEngine.UIElements;
+using UnityEngine.SceneManagement;
 
 public class HUD : MonoBehaviour
 {
     // private UIDocument _attachedDocument = null;
     // private VisualElement _root = null;
 
-    [SerializeField] private Text text;
-    private Label _timer = null;
+    public Text text;
+    public GameObject _timer = null;
 
     float _elapsedTime = 0;
-    int _timerSeconds = 0;
+    public static int _timerSeconds = 0;
 
-    // void Start()
-    // {
-    //     _attachedDocument = GetComponent<UIDocument>();
-    //     if (_attachedDocument)
-    //     {
-    //         _root = _attachedDocument.rootVisualElement;
-    //     }
-    //     if (_root != null)
-    //     {
-    //         _timer = _root.Q("Timer") as Label;
-    //         _timer.text = _timerSeconds.ToString();
-    //     }
-    // }
+    public static HUD Instance;
 
-        // Update is called once per frame
+    private void Start()
+    {
+        InitializeTimer();
+        SceneManager.sceneLoaded += OnSceneLoaded;
+    }
+
+    private void OnDestroy()
+    {
+        SceneManager.sceneLoaded -= OnSceneLoaded;
+    }
+
+    private void OnSceneLoaded(Scene scene, LoadSceneMode mode)
+    {
+        InitializeTimer();
+    }
+
+    private void InitializeTimer()
+    {
+        _timer = GameObject.FindWithTag("Timer");
+        if (_timer != null)
+        {
+            text = _timer.GetComponent<Text>();
+        }
+        else
+        {
+            Debug.LogError("Timer TextMeshProUGUI not found in the scene.");
+        }
+    }
+
+    private void Awake()
+    {
+        Instance = this;
+        DontDestroyOnLoad(gameObject);
+    }
+
     void Update()
     {
         _elapsedTime += Time.deltaTime;
-        if (_elapsedTime > _timerSeconds + 1)
+        if (_elapsedTime > _timerSeconds + 1 && SceneManager.GetActiveScene().name != "WinScreen")
         {
             _timerSeconds += 1;
             text.text = _timerSeconds.ToString();
+        }
+        else if (SceneManager.GetActiveScene().name == "WinScreen")
+        {
+            text.text = "TIME: " + _timerSeconds.ToString() + "s";
         }
     }
 }
